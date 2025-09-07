3dbf559e6309252b5c36b9c0f89fbcf6dbe01c77
Vinicyus-Alves <168252000+VinicyusMarins@users.noreply.github.com>
2025-09-06 23:10:18 -0300

    bug corrigido aplicação finalizada

--- src/services/password/handle.js
+++ src/services/password/handle.js
@@ -1,32 +1,33 @@
 async function handle() {
     let character = await permitedChar();
     let password = "";
-    const passwordLength = process.env.PASSWORD_LENGTH;
+    const passwordLength = parseInt(process.env.PASSWORD_LENGTH);
 
     for(let i=0; i<passwordLength; i++){
         let index = Math.floor(Math.random() * character.length);
         password += character[index];
     }
 
-    return character;
+    return password;
 }
 
 async function permitedChar() {
-    let permited = [];
-    if (process.env.UPPERCASE_LETTER === "true") {
-        permited.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    }
-    if(process.env.LOWERCASE_LETTER === "true"){
-        permited.push(..."abcdefghijklmnopqrstuvwxyz");
-    }
-    if(process.env.NUMBER === "true"){
-        permited.push(..."0123456789");
-    }
-    if(process.env.SPECIAL_CHAR === "true"){
-        permited.push(..."!@#$%&[]");
-    }
+    const chars = [];
 
-    return permited;
+  if (process.env.UPPERCASE_LETTER === "true") {
+    chars.push(...'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  }
+  if (process.env.LOWERCASE_LETTER === "true") {
+    chars.push(...'abcdefghijklmnopqrstuvwxyz');
+  }
+  if (process.env.NUMBER === "true") {
+    chars.push(...'0123456789');
+  }
+  if (process.env.SPECIAL_CHAR === "true") {
+    chars.push(...'!@#$%&*()_-+=[]{}?/');
+  }
+
+  return chars;
 }
 
 export default handle;
